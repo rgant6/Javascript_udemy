35d624b47000eebef609932aafad4a46a6b2d37c
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-02-17 10:24:56 -0600

    Update scope2.js

--- basics/scope2.js
+++ basics/scope2.js
@@ -1,13 +1,16 @@
-let name = "Ryan"
+// let name = "Ryan"
 
 // Name resets outside of each scope
 
 if (true){
-    let name = "Mike"
+    // let name = "Mike"
     if (true) {
-        
-        console.log(name)
-    }   
+        // name = "Jen"
+        let name = "Jen"
+        // Assigning a new value "name" here is creating a new global known as a leaked global
+        // This is an issue since no let is seen here
+        console.log(name) 
+    } 
 }
 if (true) {
     console.log(name)
