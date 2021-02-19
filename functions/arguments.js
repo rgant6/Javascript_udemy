6ec44f7863ee4f8f4301034c5a5512b184470306
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-02-18 22:10:15 -0600

    Final updates I promise

--- functions/arguments.js
+++ functions/arguments.js
@@ -1,6 +1,13 @@
-let add = function (a,b) {
-
+let add = function (a,b,c) {
+    return a+b+c
 }
 
-let result = add(10)
+let result = add(10,1,5)
 console.log(result)
+
+let getScoreText = function (name,score=0) {
+    console.log(name)
+    console.log(score)
+}
+
+getScoreText()
