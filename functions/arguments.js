4272b69402c8363c3afb6de105bafdc2b73b03ac
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-02-18 22:17:02 -0600

    Update arguments.js

--- functions/arguments.js
+++ functions/arguments.js
@@ -9,5 +9,14 @@ let getScoreText = function (name = "Anonymous",score=0) {
   return "Name: "+name+ " | Score: "+score
 }
 
-let scoreText = getScoreText("Ryan")
+let scoreText = getScoreText(undefined,99)
 console.log(scoreText)
+
+let getTip = function (money,percent=20){
+    let tip = money * (percent/100)
+    return tip
+}
+
+console.log("$"+getTip(40,15))
+console.log("$"+getTip(100,10))
+
