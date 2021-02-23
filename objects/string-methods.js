4b9c4bcdf52974727291a4bc7fe3d1d18220fb06
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-02-22 22:07:16 -0600

    Update string-methods.js

--- objects/string-methods.js
+++ objects/string-methods.js
@@ -2,3 +2,24 @@ let name = "Ryan Gant"
 
 // length property
 console.log(name.length)
+
+// Convert to upperCase
+console.log(name.toUpperCase())
+
+// Convert to lower case
+console.log(name.toLowerCase())
+
+let password = 'abc123password'
+
+// console.log(password.includes('password'))
+
+let isValidPassword = function (password){
+    return (!password.toUpperCase().includes('PASSWORD') && password.length > 8)
+}
+
+console.log(isValidPassword('asdfp'))
+console.log(isValidPassword('123adseredvd123'))
+console.log(isValidPassword('asdfppassword123'))
+
+
+// MDN web dos is documentation for methods
