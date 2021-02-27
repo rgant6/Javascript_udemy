a5cb6a8b95aec3918c35a895d762fbe995a4d0e7
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-02-27 10:08:23 -0600

    starting with Array Functions

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,15 @@
-const notes = ["Note 1","Note 2","Note 3"]
+// array of objects
+const notes = [{},{
+    title: "My next trip",
+    body: "I would like to go to Spain"
+},{
+    title: "Habits to work on",
+    body: "Eat healthy"
+},{
+    title: "Home Improvments",
+    body: "Build Porch"
+}]
+
 // MDN array is the documentation
 
 // adds a new item to the end
@@ -16,34 +27,50 @@ const notes = ["Note 1","Note 2","Note 3"]
 // it also allows you to move forward in the array if you do 0 for second arguement
 // notes.splice(1,1)
 // splice allows you to add and remove
-// notes.splice(1,1,"New Item")
+// // notes.splice(1,1,"New Item")
 
-notes[2] = "This is the new note"
+// notes[2] = "This is the new note"
 
-// this is a for loop looping over the item in the array
-notes.forEach(function (x,index){
-    console.log(index)
-    console.log(x)
-})
+// // this is a for loop looping over the item in the array
+// notes.forEach(function (x,index){
+//     console.log(index)
+//     console.log(x)
+// })
+
+// // counting-for loop with the variable and then condition after semi colon then the function after the next semi colon
+// for (let count = 0; count<=2; count++){
+//     console.log(`counting: ${count}...`)
+// }
+
+// for (let count = 2; count >= 0; count--) {
+//     console.log(`counting: ${count}...`)
+// }
+
+// // note: count is scoped locally
+// // coping the for each below
+// for (let count = 0; count< notes.length; count ++){
+//     console.log(notes[count])
+// }
+
+// for (let count = notes.length-1; count >= 0; count--) {
+//     console.log(notes[count])
+// }
+
+// // search for location of object in array; negative 1 returns for non-existing objects
+// console.log("position: ",notes.indexOf("Note 7"))
 
 console.log(notes.length)
 console.log(notes)
+console.log(notes.indexOf({}))
 
-// counting-for loop with the variable and then condition after semi colon then the function after the next semi colon
-for (let count = 0; count<=2; count++){
-    console.log(`counting: ${count}...`)
-}
+// {} === {} will return false
+// But the below will show true
+let someObject = {}
+let otherObject = someObject
+console.log(otherObject === someObject)
 
-for (let count = 2; count >= 0; count--) {
-    console.log(`counting: ${count}...`)
-}
-
-// note: count is scoped locally
-// coping the for each below
-for (let count = 0; count< notes.length; count ++){
-    console.log(notes[count])
-}
-
-for (let count = notes.length-1; count >= 0; count--) {
-    console.log(notes[count])
-}
+const index = notes.findIndex(function (note, index){
+    console.log(index)
+    return note.title ==="Habits to work on"
+})
+console.log(index)
--- arrays/todo.js
+++ arrays/todo.js
@@ -4,10 +4,12 @@ todo.splice(2,1)
 todo.push("Clean the garage")
 todo.shift()
 
-
-
 console.log(`You have ${todo.length} tasks to do today`)
 
 todo.forEach(function (items,index){
     console.log(`${index+1}. ${items}`)
 })
+// recreating the above foreach
+for (let count = 0; count <= todo.length-1; count ++){
+    console.log(`${count + 1}. ${todo[count]}`)
+}
