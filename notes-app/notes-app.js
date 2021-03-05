84e04e7af4935fbdbe18ce13f14f835f91bb7405
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-03-05 13:03:26 -0600

    updates

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,32 @@
+const notes = [{
+    title: "My next trip",
+    body: "I would like to go to Spain"
+}, {
+    title: "Habits to work on",
+    body: "Eat healthy"
+}, {
+    title: "Home Improvments next",
+    body: "Build Porch"
+}]
+
+// event handlers
+document.querySelector("#create").addEventListener('click',function (e) {
+    // changes the element for the actual target
+    // e.target.textContent = "The button was clicked"
+    let title = window.prompt("Please enter a note title")
+    const newNote = document.createElement('p')
+    newNote.className = "note"
+    newNote.textContent = title
+    document.querySelector("#create").before(newNote)
+})
+
+document.querySelector("#remove").addEventListener('click', function (){
+// #is for id and . is for classes   
+    document.querySelectorAll("p.note").forEach(function (note){
+        note.remove()
+    })
+})
+
 // DOM - Document Object Model
 
 // representation of the paragraph element
@@ -6,11 +35,19 @@
 // p.remove()
 
 // query all and remove
-const ps = document.querySelectorAll("p")
+// const ps = document.querySelectorAll("p")
 // iterating over all paragraph tags and deleting them
-ps.forEach(function (p){
+// ps.forEach(function (p){
     // replaces the text with the **** in each paragraph
-    p.textContent = "*******"
+    // p.textContent = "*******"
     // prints the text content of paragraph
     // console.log(p.textContent)
-})
+// })
+
+// add a new element
+// createelement('element')
+// const newParagraph = document.createElement('p')
+// newParagraph.textContent = 'Here is a new paragraph added by DOM'
+// document.querySelector('body').appendChild(newParagraph)
+
+// The code above doesn't see the paragraph just added so it doesn't censor it
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,8 +1,74 @@
-const ps = document.querySelectorAll("p")
+const todo = [{
+    title: "Make the bed",
+    completed: true
+}, {
+    title: "Eat Breakfast",
+    completed: false
+}, {
+    title: "Make Coffee",
+    completed: true
+}, {
+    title: "Go shopping",
+    completed: false
+}, {
+    title: "Eat Dinner",
+    completed: false
+}]
 
-ps.forEach(function(ps) {
-    console.log(ps.textContent.indexOf("the"))
-    if (ps.textContent.indexOf("the") > -1){
-        ps.remove()
-    }
+// const ps = document.querySelectorAll("p")
+
+// ps.forEach(function(ps) {
+//     console.log(ps.textContent.indexOf("the"))
+//     if (ps.textContent.indexOf("the") > -1){
+//         ps.remove()
+//     }
+// })
+
+// Challenge: Iterate over the amount of todos left
+// You have 2 todos left (in a paragraph)
+// Add a p for each value above
+
+// How he did it:
+// const incompleteTodos = todos.filter(function (todo){
+//     return !todo.completed
+// })
+
+// const summary = document.createElement('h2')
+// summary.textContent = `You have ${incompleteTodos.length} todos left`
+// document.querySelector('body').appendChild(summary)
+
+let count = 0
+todo.forEach(function (todo){
+    console.log(todo.completed)
+    if (!todo.completed){
+        count ++
+    } 
 })
+
+document.querySelector('#create_todo').addEventListener('click', function (e) {
+    // changes the element for the actual target
+    // e.target.textContent = "The button was clicked"
+    console.log("I'm adding a new todo")
+})
+
+console.log(count)
+let texter = ''
+let texter1 = ''
+
+if (count>1){
+    texter = "tasks"
+    texter1 = "are"
+}else{
+    texter = "task"
+    texter1 = "is"
+}
+
+const newParagraph = document.createElement('h2')
+newParagraph.textContent = `There ${texter1} ${count} ${texter} left to finish`
+newParagraph.querySelector('body').appendChild(newParagraph)
+
+todo.forEach(function (todos){
+    const newPara = document.createElement('p')
+    newPara.textContent = todos.title
+    document.querySelector('body').appendChild(newPara)
+});
