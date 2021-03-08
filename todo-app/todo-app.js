c5a61f695414436bc0ef0315881bbd30f06a9dae
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-03-08 11:38:11 -0600

    Challenge build

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -18,10 +18,10 @@ const todo = [{
 // const ps = document.querySelectorAll("p")
 
 // ps.forEach(function(ps) {
-//     console.log(ps.textContent.indexOf("the"))
-//     if (ps.textContent.indexOf("the") > -1){
-//         ps.remove()
-//     }
+// console.log(ps.textContent.indexOf("the"))
+// if (ps.textContent.indexOf("the") > -1){
+// ps.remove()
+// }
 // })
 
 // Challenge: Iterate over the amount of todos left
@@ -30,23 +30,22 @@ const todo = [{
 
 // How he did it:
 // const incompleteTodos = todos.filter(function (todo){
-//     return !todo.completed
+// return !todo.completed
 // })
 
 // const summary = document.createElement('h2')
 // summary.textContent = `You have ${incompleteTodos.length} todos left`
 // document.querySelector('body').appendChild(summary)
 
-let count = 0
-todo.forEach(function (todo){
-    console.log(todo.completed)
-    if (!todo.completed){
-        count ++
-    } 
-})
+document
 
-document.querySelector("#new_todo").addEventListener('input',function(e){
-    console.log(e.target.value)
+
+let count = 0
+todo.forEach(function (todo) {
+    console.log(todo.completed)
+    if (!todo.completed) {
+        count++
+    }
 })
 
 document.querySelector('#create_todo').addEventListener('click', function (e) {
@@ -59,10 +58,10 @@ console.log(count)
 let texter = ''
 let texter1 = ''
 
-if (count>1){
+if (count > 1) {
     texter = "tasks"
     texter1 = "are"
-}else{
+} else {
     texter = "task"
     texter1 = "is"
 }
@@ -71,8 +70,27 @@ const newParagraph = document.createElement('h2')
 newParagraph.textContent = `There ${texter1} ${count} ${texter} left to finish`
 newParagraph.querySelector('body').appendChild(newParagraph)
 
-todo.forEach(function (todos){
+todo.forEach(function (todos) {
     const newPara = document.createElement('p')
     newPara.textContent = todos.title
-    document.querySelector('body').appendChild(newPara)
-});
+    document.querySelector('#new_todos').appendChild(newPara)
+    newPara.className = "todo"
+})
+
+document.querySelector("#filter_todo").addEventListener('click',function (e){
+    console.log("Add a new todo...")
+})
+
+const renderedTodos = function (todos, filter) {
+    const filteredItems = notes.filter(function (todo) {
+        todo.title.toLowerCase.includes(filters.searchText.toLowerCase())
+    })
+    document.querySelector('#new_todos').innerHTML = ''
+    filteredItems.forEach(function (todo) {
+        const p1 = document.createElement('p')
+        p1.textContent = todo.title
+        p1.className = "todo"
+
+        document.querySelector("#new_todos").appendChild(p1)
+    })
+}
