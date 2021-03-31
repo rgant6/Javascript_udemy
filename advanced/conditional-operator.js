efa96398a1ad645e4df182f4cd6adde4a18708df
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-03-30 20:33:13 -0500

    Create conditional-operator.js

---
+++ advanced/conditional-operator.js
@@ -0,0 +1,21 @@
+// const myAge = 17
+// const message = myAge >= 18 ? 'You can vote!' : 'You cannot vote' 
+// // coniditional operators
+// // if (userAge >= 18){
+// //     message = 'You can vote.'
+// // }else{
+// //     message = 'You cannot vote.'
+// // }
+// console.log(message)
+
+const myAge = 17
+
+const showPage = () => {
+    return 'Showing the page'
+}
+const showErrorPage = () => {
+    return "showing the error page"
+}
+
+const message = myAge >= 21 ? showPage(): showErrorPage()
+console.log(message)
