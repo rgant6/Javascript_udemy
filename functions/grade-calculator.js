171cf13c2b6a796fb8e2646453f63d127814f940
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-03-31 17:45:48 -0500

    try catch building

--- functions/grade-calculator.js
+++ functions/grade-calculator.js
@@ -1,18 +1,26 @@
 const gradeCalculator = function (studentScore,totalScore){
-    const score = (studentScore/totalScore)*100
-    let grade = ""
-    
-    if (score >= 90 ){
-        grade = "A"
-    }else if(score >= 80){
-        grade = "B"
-    }else if(score >= 70){
-        grade = "C"
-    }else if(score >= 60){
-        grade = "D"
-    }else {
-        grade = "F"
+    if (typeof studentScore === 'number' && typeof totalScore === 'number'){
+        const score = (studentScore/totalScore)*100
+        let grade = ""
+        
+        if (score >= 90 ){
+            grade = "A"
+        }else if(score >= 80){
+            grade = "B"
+        }else if(score >= 70){
+            grade = "C"
+        }else if(score >= 60){
+            grade = "D"
+        }else {
+            grade = "F"
+        }
+        return `You got a(n) ${grade} with a score of ${score}%.`
+    }else{
+        throw Error('Error seen: both arguments should be numbers')
     }
-    return `You got a(n) ${grade} with a score of ${score}%.`
 }
-console.log(gradeCalculator(20,50))
+try{
+    console.log(gradeCalculator('hi',50))
+}catch (e){
+    console.log('Error here')
+}
