2208cd732db801a86d0fc9c874f80950f8872d6f
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-03-31 22:00:20 -0500

    Putting try catch into both apps

--- functions/grade-calculator.js
+++ functions/grade-calculator.js
@@ -1,22 +1,23 @@
 const gradeCalculator = function (studentScore,totalScore){
-    if (typeof studentScore === 'number' && typeof totalScore === 'number'){
-        const score = (studentScore/totalScore)*100
-        let grade = ""
-        
-        if (score >= 90 ){
-            grade = "A"
-        }else if(score >= 80){
-            grade = "B"
-        }else if(score >= 70){
-            grade = "C"
-        }else if(score >= 60){
-            grade = "D"
-        }else {
-            grade = "F"
-        }
-        return `You got a(n) ${grade} with a score of ${score}%.`
-    }else{
-        throw Error('Error seen: both arguments should be numbers')
+    if (typeof studentScore !== 'number' || typeof totalScore !== 'number'){
+        throw Error("Please provide numbers only")
+    }
+    const score = (studentScore/totalScore)*100
+    let grade = ""
+    
+    if (score >= 90 ){
+        grade = "A"
+    }else if(score >= 80){
+        grade = "B"
+    }else if(score >= 70){
+        grade = "C"
+    }else if(score >= 60){
+        grade = "D"
+    }else {
+        grade = "F"
+    }
+    return `You got a(n) ${grade} with a score of ${score}%.`
+    
     }
 }
 try{
