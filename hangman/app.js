967fdca8177771c26b3ca05fb323803ba176a29d
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-04-06 20:30:11 -0500

    Finishing primitive objects

--- hangman/app.js
+++ hangman/app.js
@@ -1,11 +1,14 @@
 // Primitive Values: string, number, boolean, null, undefined
 
-
 // Prototype chain for an object
 // myObject --> Object.prototype --> null
 // Array: myArray --> aAray.prototype --> Object.prototype --> null
 // Function: myFunc --> Function.prototype --> Object.prototype --> null
-// String: myStr --> String.prototype --> Objet.prototype --> null
+// String: myString --> String.prototype --> Objet.prototype --> null
+// Number: myNumber --> Number.prototype --> Objet.prototype --> null
+// Boolean: myBoolean --> Boolean.prototype --> Objet.prototype --> null
+
+
 
 const product = 'Computer'
 console.log(product)
