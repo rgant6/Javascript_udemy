44ca4c670a802197a6722502c2e52d05425a60ed
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-04-10 16:12:40 -0500

    Switched from console to DOM elements for hangman

--- hangman/app.js
+++ hangman/app.js
@@ -8,10 +8,48 @@
 // Number: myNumber --> Number.prototype --> Objet.prototype --> null
 // Boolean: myBoolean --> Boolean.prototype --> Objet.prototype --> null
 
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
+    this.word.forEach((letter) => {
+        puzzle += this.guessedLetters.includes(letter) ? letter : ((letter === ' ') ? ' ' : '*')
+    })
+    return puzzle
+}
 
+Hangman.prototype.guess = function (letterGuess) {
+    const letter = letterGuess.toLowerCase()
+    if (!this.guessedLetters.includes(letter)) {
 
-const product = 'Computer'
-console.log(product)
+        this.guessedLetters += letter
+        if (!this.word.includes(letter)) {
+            this.remainingGuesses -= 1
+        }
+    }
+}
 
-const otherProduct = new String('Phone')
-console.log(otherProduct)
+const refreshGame = function (game) {  
+
+    const gameEl = document.createElement('div')
+    gameEl.className = "hangman"
+
+    const textEl1 = document.createElement('a')
+    const textEl2 = document.createElement('a')
+    textEl1.textContent = game.getPuzzle()
+    textEl2.textContent = `Remaining guesses: ${game.remainingGuesses}`
+    gameEl.appendChild(textEl1)
+    linebreak = document.createElement("br");
+    gameEl.appendChild(linebreak)
+    gameEl.appendChild(textEl2)
+    document.querySelector('#hangman').appendChild(gameEl)
+}
+
+game1 = new Hangman("california", 20)
+refreshGame(game1)
+game2 = new Hangman("bus", 6)
+
+window.addEventListener('keypress', function (e) {
+    const guess = String.fromCharCode(e.charCode)
+    game1.guess(guess)
+    refreshGame(game1)
+
+})
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -4,34 +4,4 @@ const Hangman = function (word, remainingGuesses, guessedLetters = []){
     this.guessedLetters = guessedLetters
 }
 
-Hangman.prototype.getPuzzle = function() {
-    let puzzle = ''
-    this.word.forEach((letter) => {
-        puzzle += this.guessedLetters.includes(letter) ? letter:((letter === ' ') ? ' ': '*')
-    })
-    return puzzle    
-}
 
-Hangman.prototype.guess = function (letterGuess) {
-    const letter = letterGuess.toLowerCase()
-    if(!this.guessedLetters.includes(letter)){
-        
-        this.guessedLetters += letter
-        if (!this.word.includes(letter)){
-            this.remainingGuesses -= 1
-        }
-    }
-}
-
-game1 = new Hangman("california",20)
-console.log(game1.getPuzzle())
-console.log(game1.remainingGuesses)
-game2 = new Hangman("bus",6)
-
-window.addEventListener('keypress', function (e) {
-    const guess = String.fromCharCode(e.charCode)
-    game1.guess(guess)
-    console.log(game1.getPuzzle())
-    console.log(game1.remainingGuesses)
-
-})
