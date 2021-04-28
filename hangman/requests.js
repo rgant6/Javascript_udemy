05da53b4601fef2b623a266e40602b0ad7152ab9
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-04-28 17:40:06 -0500

    Small format changes

--- hangman/requests.js
+++ hangman/requests.js
@@ -8,17 +8,7 @@ const getPuzzle = async (wordCount) => {
     }
 }
 
-const getPuzzleOld = (wordCount) => {
-    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
-        if (response.status === 200) {
-            return response.json()
-        } else {
-            throw new Error('Unable to fetch puzzle')
-        }
-    }).then((data) => {
-        return data.puzzle
-    })
-}
+
 
 const getCountry = (countryCode) => {
     return fetch(`https://restcountries.eu/rest/v2/all`).then((response) => {
@@ -50,3 +40,16 @@ const getLocation = () => {
 // Could also use the below instead of a forEach loop
 // const country = data.find((country) => country.alpha2Code === countryCode)
 // return country.name
+
+// Promises without Async/Await
+// const getPuzzleOld = (wordCount) => {
+//     return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
+//         if (response.status === 200) {
+//             return response.json()
+//         } else {
+//             throw new Error('Unable to fetch puzzle')
+//         }
+//     }).then((data) => {
+//         return data.puzzle
+//     })
+// }
