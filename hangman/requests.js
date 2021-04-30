2bde1d694536379019d9af8ceb22e973511f06a2
rgant6 <55454627+rgant6@users.noreply.github.com>
2021-04-29 20:21:14 -0500

    Updating getCountry and getLocation to async/await

--- hangman/requests.js
+++ hangman/requests.js
@@ -8,33 +8,28 @@ const getPuzzle = async (wordCount) => {
     }
 }
 
-
-
-const getCountry = (countryCode) => {
-    return fetch(`https://restcountries.eu/rest/v2/all`).then((response) => {
-        if (response.status === 200){
-            return response.json()
-        } else{
-            throw new Error(`Unable to fetch the country`)
-        }
-    }).then((data) => {
+const getCountry = async (countryCode) => {
+    const response = await fetch(`https://restcountries.eu/rest/v2/all`)
+    if (response.status === 200){
+        const data = await response.json()
         data.forEach(country => {
-            if (country.alpha2Code === countryCode){
+            if (country.alpha2Code === countryCode) {
                 countryRet = country.name
             }
         })
-        return countryRet
-    })
+        return countryRet 
+    }else{
+        throw new Error(`Unable to fetch the country`)
+    }   
 }
 
-const getLocation = () => {
-    return fetch(`https://ipinfo.io/json?token=82102276a94b17`).then((response) => {
+const getLocation = async () => {
+    const response = await fetch(`https://ipinfo.io/json?token=82102276a94b17`)
         if (response.status === 200) {
             return response.json()
         } else {
             throw new Error(`Unable to fetch your location`)
         }
-    })
 }
 
 // Could also use the below instead of a forEach loop
